51eeaf949b3fc1e0fd28c1b8a5976a1244f0bcb1
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-06-06 12:50:36 +0300

    Add an installer script.

---
+++ release.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+REM This script requires version 6.0.0 of Inno Setup to be added to PATH.
+rmdir published /S /Q
+dotnet publish Source\DayGame.csproj -c Release -o published /p:PublishTrimmed=false -r win-x86 /bl
+iscc DayGame.iss

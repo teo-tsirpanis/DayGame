718f157181d8fc095af51c99363836070ef137c2
Mitro <mitrontatidis@gmail.com>
2020-06-06 12:34:12 +0300

    PrepareForBoss initial

---
+++ Source/PrepareForBoss.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DayGame
+{
+    public partial class PrepareForBoss : Form
+    {
+        public PrepareForBoss(SaveFile sf)
+        {
+            InitializeComponent();
+            // BossBattleFrame objForm = new BossBattleFrame(new Character("dafd", "daf"), new Inventory(), new Boss("fassdf", null, 60, 2, 7));
+            InventoryGUI objForm = new InventoryGUI(sf);
+            objForm.TopLevel = false; 
+            panel1.Controls.Add(objForm);
+            objForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            objForm.Dock = DockStyle.Fill;
+            objForm.Show();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}

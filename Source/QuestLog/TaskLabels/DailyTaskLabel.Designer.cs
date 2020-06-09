d4ecc1a3fcec21c1899bf4b26177bdcf5b553c60
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-06-09 22:50:42 +0300

    Refactor the DailyTaskLabel class.
    Make it properly behave on dailies that were done for today.

--- Source/QuestLog/TaskLabels/DailyTaskLabel.Designer.cs
+++ Source/QuestLog/TaskLabels/DailyTaskLabel.Designer.cs
@@ -32,29 +32,27 @@ namespace DayGame
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DailyTaskLabel));
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.doneCheckBox = new System.Windows.Forms.CheckBox();
             this.descriptionLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
             this.nameLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (247)))), ((int) (((byte) (163)))), ((int) (((byte) (27)))));
-            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.checkBox1.Location = new System.Drawing.Point(0, 0);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(27, 49);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox1.UseVisualStyleBackColor = false;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
-            // 
+            //
+            // doneCheckBox
+            //
+            this.doneCheckBox.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (247)))), ((int) (((byte) (163)))), ((int) (((byte) (27)))));
+            this.doneCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.doneCheckBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.doneCheckBox.Location = new System.Drawing.Point(0, 0);
+            this.doneCheckBox.Name = "doneCheckBox";
+            this.doneCheckBox.Size = new System.Drawing.Size(27, 49);
+            this.doneCheckBox.TabIndex = 0;
+            this.doneCheckBox.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.doneCheckBox.UseVisualStyleBackColor = false;
+            //
             // descriptionLabel
-            // 
+            //
             this.descriptionLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.descriptionLabel.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.descriptionLabel.ForeColor = System.Drawing.Color.Gray;
@@ -64,30 +62,30 @@ namespace DayGame
             this.descriptionLabel.TabIndex = 1;
             this.descriptionLabel.Text = "label1";
             this.descriptionLabel.Click += new System.EventHandler(this.descriptionLabel_Click);
-            // 
+            //
             // panel1
-            // 
+            //
             this.panel1.BackColor = System.Drawing.Color.LightGray;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 48);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(292, 1);
             this.panel1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = ((System.Drawing.Image) (resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(268, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(21, 22);
-            this.button1.TabIndex = 7;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
+            //
+            // deleteButton
+            //
+            this.deleteButton.FlatAppearance.BorderSize = 0;
+            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteButton.Image = ((System.Drawing.Image) (resources.GetObject("deleteButton.Image")));
+            this.deleteButton.Location = new System.Drawing.Point(268, 26);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(21, 22);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.button1_Click);
+            //
             // nameLabel
-            // 
+            //
             this.nameLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.nameLabel.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (65)))), ((int) (((byte) (198)))), ((int) (((byte) (144)))));
             this.nameLabel.Location = new System.Drawing.Point(24, 0);
@@ -95,26 +93,26 @@ namespace DayGame
             this.nameLabel.Size = new System.Drawing.Size(265, 12);
             this.nameLabel.TabIndex = 11;
             this.nameLabel.Text = "nameLabel";
-            // 
+            //
             // DailyTaskLabel
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(292, 49);
             this.Controls.Add(this.nameLabel);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.descriptionLabel);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.doneCheckBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "DailyTaskLabel";
             this.Text = "DailyTaskLabel";
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.CheckBox doneCheckBox;
         private System.Windows.Forms.Label descriptionLabel;
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Panel panel1;
--- Source/QuestLog/TaskLabels/DailyTaskLabel.cs
+++ Source/QuestLog/TaskLabels/DailyTaskLabel.cs
@@ -22,35 +22,18 @@ namespace DayGame
             descriptionLabel.Text = descr.Length > 50 ? $@"{descr.Substring(0, 50)}..." : descr;
             nameLabel.Text = daily.Name;
 
-            if (saveFile.SaveDate.Date < DateTime.Today.Date)
+            int days = (DateTime.Today - saveFile.SaveDate).Days;
+
+            if (daily.DoneForToday)
             {
-                int days;
-
-                if (daily.DoneForToday)
-                {
-                    days = ((int) (DateTime.Today - saveFile.SaveDate).TotalDays) - 1;
-                    daily.DoneForToday = false;
-                }
-                else
-                {
-                    days = ((int) (DateTime.Today - saveFile.SaveDate).TotalDays);
-                }
-
-                for (int i = 0; i < days; i++)
-                {
-                    daily.UpdateTask(false, character);
-                }
+                days--;
+                daily.DoneForToday = false;
+                doneCheckBox.BackColor = Color.LightGray;
+                doneCheckBox.Enabled = false;
+                doneCheckBox.Checked = true;
             }
-            else
-            {
-                checkBox1.BackColor = Color.LightGray;
-                checkBox1.Checked = true;
-            }
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
+            for (int i = 0; i < days; i++) daily.UpdateTask(false, character);
+            doneCheckBox.CheckedChanged += DoneCheckBoxClick;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -64,10 +47,10 @@ namespace DayGame
             MessageBox.Show(daily.Description, daily.Name);
         }
 
-        private void checkBox1_Click(object sender, EventArgs e)
+        private void DoneCheckBoxClick(object sender, EventArgs e)
         {
-            checkBox1.BackColor = Color.LightGray;
-            checkBox1.Enabled = false;
+            doneCheckBox.BackColor = Color.LightGray;
+            doneCheckBox.Enabled = false;
             daily.UpdateTask(true, character);
             daily.DoneForToday = true;
             onUpdate();

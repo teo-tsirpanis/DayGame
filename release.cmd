317895a397aa4e6c0f57bccb692dc26aca1d85ad
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-05-15 11:48:55 +0300

    Switch to a framework-dependent deployment. DayGame is a release-and-forget project; the self-contained deployment model requires regular patching which cannot be done. With framework-dependent deployments, the .NET runtime will get patched with Windows Update.

--- release.cmd
+++ release.cmd
@@ -2,5 +2,5 @@
 
 REM This script requires version 6.0.0 of Inno Setup to be added to PATH.
 rmdir published /S /Q
-dotnet publish Source\DayGame.csproj -c Release -o published /p:PublishTrimmed=true -r win-x86
+dotnet publish Source\DayGame.csproj -c Release -o published --no-self-contained
 iscc DayGame.iss
